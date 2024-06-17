84f1aee63d3a77c890992963bae744afc1b85370
saviorxzero98 <saviorxzero98@gmail.com>
2024-06-17 18:01:55 +0800

    add: CamelCase Keys Sample

---
+++ 003-Packages/009-JsonAndYaml/CamelCaseKeys/Readme.md
@@ -0,0 +1,7 @@
+
+# Camel Case Keys
+
+處理 JSON 字串轉換成 JavaScript 物件時，會因為 Property Name 大小寫問題導致解析的問題
+
+* [camelcase-keys](https://github.com/sindresorhus/camelcase-keys)
+
---
+++ 003-Packages/009-JsonAndYaml/CamelCaseKeys/dist/index.js
@@ -0,0 +1,95 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const camelcaseKeys = require("camelcase-keys");
+let demoObjects = {
+    'snake_case': {
+        'id': 1,
+        'user_name': 'one',
+        'data': {
+            'level': 1,
+            'create_date': new Date()
+        },
+        'value': {
+            'Id': 2,
+            'Name': 'two'
+        },
+        'channel_data': {
+            'Id': 2,
+            'Name': 'two'
+        }
+    },
+    'kebab-case': {
+        'id': 1,
+        'user-name': 'one',
+        'data': {
+            'level': 1,
+            'create-date': new Date()
+        },
+        'value': {
+            'Id': 2,
+            'Name': 'two'
+        },
+        'channel-data': {
+            'Id': 2,
+            'Name': 'two'
+        }
+    },
+    'camelCase': {
+        'id': 1,
+        'userName': 'one',
+        'data': {
+            'level': 1,
+            'createDate': new Date()
+        },
+        'value': {
+            'Id': 2,
+            'Name': 'two'
+        },
+        'channelData': {
+            'Id': 2,
+            'Name': 'two'
+        }
+    },
+    'PascalCase': {
+        'Id': 1,
+        'UserName': 'one',
+        'Data': {
+            'Level': 1,
+            'CreateDate': new Date()
+        },
+        'Value': {
+            'Id': 2,
+            'Name': 'two'
+        },
+        'ChannelData': {
+            'Id': 2,
+            'Name': 'two'
+        }
+    },
+    'Pascal Case': {
+        'Id': 1,
+        'User Name': 'one',
+        'Data': {
+            'Level': 1,
+            'Create Date': new Date()
+        },
+        'Value': {
+            'Id': 2,
+            'Name': 'two'
+        },
+        'Channel Data': {
+            'Id': 2,
+            'Name': 'two'
+        }
+    },
+    'nullObject': null
+};
+const demo = (name, data) => {
+    console.log(`========== ${name} ==========`);
+    console.log(`${JSON.stringify(camelcaseKeys(data, { deep: false, stopPaths: ['value', 'channelData'] }))}`);
+    console.log('\n');
+};
+for (let key of Object.keys(demoObjects)) {
+    demo(key, demoObjects[key]);
+}
+//# sourceMappingURL=index.js.map
